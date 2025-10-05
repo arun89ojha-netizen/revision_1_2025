41cf1ada1e6cc100f5f8021cf0fe70f65f6f7e8e
Ashish <arun89ojha@gmail.com>
2025-10-04 23:50:25 -0700

    added project

---
+++ Revision_1_oct_2025/src/test/java/revisonPackage/Test1.java
@@ -0,0 +1,24 @@
+package revisonPackage;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test1 {
+	
+	@Test
+	public void a() {
+		System.out.println("hello a test method");
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+		driver.get("https://www.facebook.com");
+	}
+
+}
